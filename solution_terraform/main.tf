7e89c5428fdc45484f080d3b011e4e1e5466ca03
javedshaikhdevops <javedshaikh.devops@gmail.com>
2025-05-12 23:22:51 +0100

    Reworked

--- solution_terraform/main.tf
+++ solution_terraform/main.tf
@@ -39,21 +39,21 @@ resource "google_privateca_ca_pool" "ca_pool" {
 resource "google_privateca_ca_pool_iam_member" "privateca_admin" {
   project = var.project_id
   location = var.region
-  ca_pool  = google_privateca_ca_pool.ca_pool.name
+  ca_pool = "projects/${var.project_id}/locations/${var.region}/caPools/${google_privateca_ca_pool.ca_pool.name}"
   role    = "roles/privateca.certificateAuthorityAdmin"
   member  = "serviceAccount:${google_service_account.privateca_service_account.email}"
 }
 
 # Create KMS resources
-resource "google_kms_key_ring" "cas_keyring_3" {
-  name     = "cas-keyring-3"
+resource "google_kms_key_ring" "cas_keyring_4" {
+  name     = "cas-keyring-4"
   location = var.region
   project  = var.project_id
 }
 
-resource "google_kms_crypto_key" "cas_key_3" {
-  name            = "cas-key-3"
-  key_ring        = google_kms_key_ring.cas_keyring_3.id
+resource "google_kms_crypto_key" "cas_key_4" {
+  name            = "cas-key-4"
+  key_ring        = google_kms_key_ring.cas_keyring_4.id
   purpose         = "ASYMMETRIC_SIGN"
   version_template {
     algorithm = "EC_SIGN_P384_SHA384"  # Recommended for CAS
@@ -66,7 +66,7 @@ resource "google_kms_crypto_key" "cas_key_3" {
 
 # Bind IAM role for the custom service account on the KMS key
 resource "google_kms_crypto_key_iam_binding" "cas_signer" {
-  crypto_key_id = google_kms_crypto_key.cas_key_3.id
+  crypto_key_id = google_kms_crypto_key.cas_key_4.id
   role          = "roles/cloudkms.signerVerifier"
   members = [
     "serviceAccount:${google_service_account.privateca_service_account.email}"
@@ -112,7 +112,7 @@ resource "google_privateca_certificate_authority" "root_ca" {
   }
 
   key_spec {
-    cloud_kms_key_version = google_kms_crypto_key.cas_key_3.id
+    cloud_kms_key_version = google_kms_crypto_key.cas_key_4.id
   }
 
   type = "SELF_SIGNED"
